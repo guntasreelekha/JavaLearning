0ea33268569270772851819a38c9f1ac9ffa52b6
guntasreelekha <guntasreelekha@gmail.com>
2020-08-29 21:59:50 +0530

    department and year of Engineering

--- DepartmentAndYearOfEngineering.java
+++ DepartmentAndYearOfEngineering.java
@@ -1,16 +1,18 @@
 package com.concent;
 
-public class DepartmentAndYear {
+public class DepartmentAndYearOfEngineering {
     public static void main(String args[]) {
         /*
          * To check the student is studying which branch and which year*/
-        String branch = "ECE";
-        int year = 3;
+        String branch = "CSE";
+        int totalYear = 4;
+        int year;
         if (branch.equals("CSE")) {
             System.out.println("Student is studying in " + branch + " Department");
-            for (year = 1; year <= 4; year++) {
+            for (totalYear = 1; totalYear <= 4; totalYear++) {
+                year = 2;
                 if (year == 1) {
-                    System.out.println("Student is studying " + year + "st year");
+                    System.out.println("Student is studying  " + year + "st year");
                     break;
                 } else if (year == 2) {
                     System.out.println("Student is studying " + year + "nd year");
@@ -18,77 +20,76 @@ public class DepartmentAndYear {
                 } else if (year == 3) {
                     System.out.println("Student is studying " + year + "rd year");
                     break;
-                } else {
+                } else if (year == 4) {
                     System.out.println("Student is studying " + year + "th year");
                     break;
+                } else {
+                    System.out.println("year " + year + " is not provided");
                 }
             }
         } else if (branch.equals("ECE")) {
             System.out.println("Student is studying in " + branch + " Department");
-            for (year = 1; year <= 4; year++) {
+            for (totalYear = 1; totalYear <= 4; totalYear++) {
+                year = 4;
                 if (year == 1) {
                     System.out.println("Student is studying " + year + "st year");
-                    break;
                 } else if (year == 2) {
                     System.out.println("Student is studying " + year + "nd year");
-                    break;
                 } else if (year == 3) {
                     System.out.println("Student is studying " + year + "rd year");
-                    break;
-                } else {
+                } else if (year == 4) {
                     System.out.println("Student is studying " + year + "th year");
-                    break;
+                } else {
+                    System.out.println("year " + year + " is not provided");
                 }
             }
         } else if (branch.equals("EEE")) {
             System.out.println("Student is studying in " + branch + " Department");
-            for (year = 1; year <= 4; year++) {
+            for (totalYear = 1; totalYear <= 4; totalYear++) {
+                year = 4;
                 if (year == 1) {
                     System.out.println("Student is studying " + year + "st year");
-                    break;
                 } else if (year == 2) {
                     System.out.println("Student is studying " + year + "nd year");
-                    break;
                 } else if (year == 3) {
                     System.out.println("Student is studying " + year + "rd year");
-                    break;
-                } else {
+                } else if (year == 4) {
                     System.out.println("Student is studying " + year + "th year");
-                    break;
+                } else {
+                    System.out.println("year " + year + " is not provided");
+
                 }
             }
         } else if (branch.equals("MEC")) {
             System.out.println("Student is studying in " + branch + " Department");
-            for (year = 1; year <= 4; year++) {
+            for (totalYear = 1; totalYear <= 4; totalYear++) {
+                year = 3;
                 if (year == 1) {
                     System.out.println("Student is studying " + year + "st year");
-                    break;
                 } else if (year == 2) {
                     System.out.println("Student is studying " + year + "nd year");
-                    break;
                 } else if (year == 3) {
                     System.out.println("Student is studying " + year + "rd year");
-                    break;
-                } else {
+                } else if (year == 4) {
                     System.out.println("Student is studying " + year + "th year");
-                    break;
+                } else {
+                    System.out.println("year " + year + " is not provided");
                 }
             }
         } else if (branch.equals("CIVIL")) {
             System.out.println("Student is studying in " + branch + " Department");
-            for (year = 1; year <= 4; year++) {
+            for (totalYear = 1; totalYear <= 4; totalYear++) {
+                year = 2;
                 if (year == 1) {
                     System.out.println("Student is studying " + year + "st year");
-                    break;
                 } else if (year == 2) {
                     System.out.println("Student is studying " + year + "nd year");
-                    break;
                 } else if (year == 3) {
                     System.out.println("Student is studying " + year + "rd year");
-                    break;
-                } else {
+                } else if (year == 4) {
                     System.out.println("Student is studying " + year + "th year");
-                    break;
+                } else {
+                    System.out.println("year " + year + " is not provided");
                 }
             }
         } else {
