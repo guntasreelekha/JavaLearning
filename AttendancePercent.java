e2df1b0c4612f46d2cb38e65553e8f145768e2f9
guntasreelekha <guntasreelekha@gmail.com>
2020-08-31 08:22:05 +0530

    Data types

---
+++ AttendancePercent.java
@@ -0,0 +1,28 @@
+package com.concent;
+
+public class Example {
+    public static void main(String args[]) {
+        char ch = 'B';
+        boolean CSE = true;
+        float totalStudents=60;
+        float presentStudentsA,presentStudentsB ;
+        float percentage;
+        if (CSE) {
+            presentStudentsA=50;
+            presentStudentsB=45;
+            if (ch == 'A') {
+                System.out.println("Total students in  CSE A :" +totalStudents);
+                percentage =(presentStudentsA/totalStudents)*100;
+                System.out.println("Weekly attendance Percentage of students in CSE-A section :" +percentage);
+            }else if(ch == 'B'){
+                System.out.println("Total students in CSE B:" +totalStudents);
+                percentage =(float)(presentStudentsB/totalStudents)*100;
+                System.out.println("Weekly attendance Percentage of students in CSE-B section :" +percentage);
+            }else{
+                System.out.println(ch+"section is not provided");
+            }
+        }else{
+            System.out.println("Another department is provided");
+        }
+    }
+}
---
+++ BoolTest.java
@@ -0,0 +1,2 @@
+package com.concent;public class BoolTest {
+}
